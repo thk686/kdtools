70d719aeef5a087ebd3441f96e6dca0155f21303
Tim Keitt <tkeitt@gmail.com>
2020-06-28 00:27:38 -0500

    bug fix

--- src/strdist.cpp
+++ src/strdist.cpp
@@ -9,21 +9,21 @@ namespace strdist {
 int levenshtein(std::string_view s1, std::string_view s2) {
   int n = s1.size() + 1, m = s2.size() + 1;
   static std::vector<int> tab;
-  static int n2 = 0, m2 = 0;
-  if (tab.size() < n * m) {
-    n2 = 2 * n; m2 = 2 * m; tab.resize(n2 * m2);
-    for (int i = 0; i != n2; ++i) tab[i *  m2] = i;
-    for (int j = 1; j != m2; ++j) tab[j] = j;
+  static int lim = 0;
+  if (lim < std::max(n, m)) {
+    lim = 2 * std::max(n, m); tab.resize(lim * lim);
+    for (int i = 0; i != lim; ++i) tab[i * lim] = i;
+    for (int j = 1; j != lim; ++j) tab[j] = j;
   }
   for (int i = 1; i != n; ++i) {
     for (int j = 1; j != m; ++j) {
       int sc = s1[i - 1] == s2[j - 1] ? 0 : 1;
-      tab[i * m2 + j] = std::min(tab[i * m2 + j - 1] + 1,
-                                 std::min(tab[(i - 1) * m2 + j] + 1,
-                                          tab[(i - 1) * m2 + j - 1] + sc));
+      tab[i * lim + j] = std::min(tab[i * lim + j - 1] + 1,
+                                  std::min(tab[(i - 1) * lim + j] + 1,
+                                           tab[(i - 1) * lim + j - 1] + sc));
     }
   }
-  return tab[(n - 1) * m2 + (m - 1)];
+  return tab[(n - 1) * lim + (m - 1)];
 }
 
 }; // namespace strdist
